cc6386fb0d07eb1dcbc3bcd871a01d71264372d2
Vladimir Gómez Fumero <vladimirg.fumero@gmail.com>
2022-08-08 00:39:54 -0400

    Apply the gateway validations

--- gateways-api/src/gateways/gateways.controller.ts
+++ gateways-api/src/gateways/gateways.controller.ts
@@ -6,19 +6,19 @@ import {
   Patch,
   Param,
   Delete,
+  UsePipes,
 } from '@nestjs/common';
 import { GatewaysService } from './gateways.service';
 import { CreateGatewayDto } from './dto/create-gateway.dto';
 import { UpdateGatewayDto } from './dto/update-gateway.dto';
-import { Document } from 'mongoose';
-import { Schema } from '@nestjs/mongoose';
-import { GatewaySchema } from '../schemas/gateways.schema';
+import { gatewaySchema, JoiValidationPipe } from '../joi-validation.pipe';
 
 @Controller('gateways')
 export class GatewaysController {
   constructor(private readonly gatewaysService: GatewaysService) {}
 
   @Post()
+  @UsePipes(new JoiValidationPipe(gatewaySchema))
   create(@Body() createGatewayDto: CreateGatewayDto) {
     return this.gatewaysService.create(createGatewayDto);
   }
