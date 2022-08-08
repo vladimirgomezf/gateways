07b2a33dabab915a94677a68cc46f9f2cc290f8f
Vladimir Gómez Fumero <vladimirg.fumero@gmail.com>
2022-08-08 02:12:24 -0400

    Create the service to add a device to a gateway

--- gateways-api/src/gateways/gateways.service.ts
+++ gateways-api/src/gateways/gateways.service.ts
@@ -1,15 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Get } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { create } from 'domain';
 import { Model } from 'mongoose';
 import { Gateway, GatewayDocument } from 'src/schemas/gateways.schema';
 import { CreateGatewayDto } from './dto/create-gateway.dto';
 import { UpdateGatewayDto } from './dto/update-gateway.dto';
+import { Device, DeviceDocument } from '../schemas/devices.schema';
 
 @Injectable()
 export class GatewaysService {
   constructor(
     @InjectModel(Gateway.name) private GatewayModel: Model<GatewayDocument>,
+    @InjectModel(Device.name) private DeviceModel: Model<DeviceDocument>
   ) {}
 
   async create(createGatewayDto: CreateGatewayDto) {
@@ -33,5 +34,15 @@ export class GatewaysService {
 
   async remove(serialNumber: string) {
     return this.GatewayModel.deleteOne({ serialNumber });
+  }
+
+  async asignDevice(serialNumber: string, uid: number) {
+    const gate = await this.GatewayModel.findOne({ serialNumber });
+    console.log("Gate: " + gate);
+    const dev = await this.DeviceModel.findOne({ uid })
+    console.log("Device: " + dev);
+    gate.devices.push(dev);
+    gate.save();
+    console.log("Final Gate: " + gate);
   }
 }
