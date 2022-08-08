291545ce2c2f545700ce28f2e9ef875e214f8679
Vladimir Gómez Fumero <vladimirg.fumero@gmail.com>
2022-08-08 02:11:54 -0400

    Add access to device model in gateway service

--- gateways-api/src/gateways/gateways.module.ts
+++ gateways-api/src/gateways/gateways.module.ts
@@ -3,9 +3,10 @@ import { GatewaysService } from './gateways.service';
 import { GatewaysController } from './gateways.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Gateway, GatewaySchema } from 'src/schemas/gateways.schema';
+import { Device, DeviceSchema } from '../schemas/devices.schema';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: Gateway.name, schema: GatewaySchema }])],
+  imports: [MongooseModule.forFeature([{ name: Gateway.name, schema: GatewaySchema }, { name: Device.name, schema: DeviceSchema }])],
   controllers: [GatewaysController],
   providers: [GatewaysService]
 })
